e9ecb945ce371de7af9d7be5f70e4790f76dee6e
Martyna Paliwoda <martynapaliwoda.2001@gmail.com>
2022-01-06 23:23:59 +0100

    DPO update

--- DPO.py
+++ DPO.py
@@ -1,3 +1,6 @@
+import networkx as nx
+
+
 def remove_from_graph(G, L, K, mapping):
     for e in L.edges:
         if e not in K.edges:
@@ -9,7 +12,7 @@ def remove_from_graph(G, L, K, mapping):
 
 
 def add_to_graph(G, K, R, mapping):
-    i = G.nodes.length
+    i = max(G.nodes) + 1
     for n in R.nodes:
         if n not in K.nodes:
             G.add_node(i, {'label': n['label']})
@@ -18,9 +21,13 @@ def add_to_graph(G, K, R, mapping):
 
     for e in R.edges:
         if e not in K.edges:
-            G.add_edge(mapping[e[0]], mapping[e[0]], {'label': e['label']})
+            G.add_edge(mapping[e[0]], mapping[e[1]], {'label': e['label']})
 
 
 def double_pushout(G, L, K, R, mapping):
     remove_from_graph(G, L, K, mapping)
     add_to_graph(G, K, R, mapping)
+
+
+G = nx.petersen_graph()
+print(G.nodes)
