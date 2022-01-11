32b5866ca6e30f6f23a67c069787d207f60ab1f8
Michal Gniadek <mgniadek@interia.pl>
2022-01-11 15:50:54 +0100

    undo

--- gui.py
+++ gui.py
@@ -5,11 +5,15 @@ from graph_selection import GraphSelection
 from input_graph import get_input_graph
 from dpo import double_pushout
 from isomorphism import get_isomorphism
+import copy
 
 
 class App:
     def __init__(self, input):
-        self.G, self.productions = get_input_graph(input)
+        G, self.productions = get_input_graph(input)
+
+        self.G_stack = [G]
+
         self.curr_idx = 0
 
         self.fig = plt.figure(figsize=(10, 8))
@@ -17,6 +21,7 @@ class App:
         self.p_axis = [self.fig.add_subplot(2, 3, 4 + i) for i in range(3)]
 
         # Saved so they won't get garbage collected
+        self.button_undo = add_button([0.10, 0.05, 0.1, 0.025], "Undo", lambda _: self.pop_graph_stack())
         self.button_prev = add_button([0.40, 0.05, 0.1, 0.025], "Previous", lambda _: self.prev())
         self.button_apply = add_button([0.5, 0.05, 0.1, 0.025], "Apply", lambda _: self.apply())
         self.button_next = add_button([0.60, 0.05, 0.1, 0.025], "Next", lambda _: self.next())
@@ -37,7 +42,7 @@ class App:
         self.draw()
 
     def get_G(self):
-        return self.G
+        return self.G_stack[-1]
 
     def get_L(self):
         return self.productions[self.curr_idx][0]
@@ -48,6 +53,11 @@ class App:
     def get_R(self):
         return self.productions[self.curr_idx][2]
 
+    def pop_graph_stack(self):
+        if len(self.G_stack) > 1:
+            self.G_stack.pop()
+            self.draw(redraw_g=True)
+
     def update_button_visibility(self):
         a = len(self.get_L().nodes) == len(self.graph_selection.get_selected())
         b = get_isomorphism(self.get_L(), self.get_K(), self.get_G(), self.graph_selection.get_selected()) is not None
@@ -56,17 +66,21 @@ class App:
     def apply(self):
         if len(self.get_L().nodes) != len(self.graph_selection.get_selected()):
             return
+
         mapping = get_isomorphism(self.get_L(), self.get_K(), self.get_G(), self.graph_selection.get_selected())
+
         if mapping is not None:
-            double_pushout(self.get_G(), self.get_L(), self.get_K(), self.get_R(), mapping)
+            new_G = copy.deepcopy(self.get_G())
+            double_pushout(new_G, self.get_L(), self.get_K(), self.get_R(), mapping)
+            self.G_stack.append(new_G)
             self.draw(redraw_g=True)
 
     def draw(self, redraw_g=False):
         if redraw_g:
             self.g_axis.clear()
-            g_layout = nx.spring_layout(self.G, k=0.1, iterations=20, seed=123)
+            g_layout = nx.spring_layout(self.get_G(), k=0.1, iterations=20, seed=123)
             self.graph_selection.update(g_layout)
-            draw_graph(self.G, g_layout, self.g_axis)
+            draw_graph(self.get_G(), g_layout, self.g_axis)
 
         for j in range(3):
             self.p_axis[j].clear()
